f2452cc2eb0fbd9825292bdbfae972630cb11697
Sudheer Peddireddy <peddireddy.sudheer@gmail.com>
2026-08-12 23:54:04 -0500

    Update frontend/app.py

--- frontend/app.py
+++ frontend/app.py
@@ -9,14 +9,14 @@ st.title("SuperKart sales forecasting")
 st.subheader("Please enter the following product details to get forecast for the sales.")
 
 # Input fields for product and store data
-Product_Weight = st.number_input("Product Weight", min_value=0.0, value=12.66)
+Product_Weight = st.number_input("Product Weight", min_value=0.0, max_value=30.0, value=15.0)
 Product_Sugar_Content = st.selectbox("Product Sugar Content", ["Low Sugar", "Regular", "No Sugar"])
-Product_Allocated_Area = st.number_input("Product_Allocated_Area", min_value=0.0, value=12.66)
-Product_MRP = st.number_input("Product_MRP", min_value=0.0, value=12.66)
+Product_Allocated_Area = st.number_input("Product_Allocated_Area", min_value=0.0, max_value=0.3, value=0.1)
+Product_MRP = st.number_input("Product_MRP", min_value=0.0, max_value=300.0, value=150.0)
 Store_Size = st.selectbox("Store_Size", ["High", "Medium", "Small"])
 Store_Location_City_Type = st.selectbox("Store_Location_City_Type", ["Tier_1", "Tier_2", "Tier_3"])
 Store_Type = st.selectbox("Store_Type", ["Departmental Store", "Supermarket Type 1", "Supermarket Type 2", "Food Mart"])
-Store_Age_Years = st.number_input("Store_Age_Years", min_value=1, value=50)
+Store_Age_Years = st.number_input("Store_Age_Years", min_value=1, max_value=50, value=20)
 Product_Type = st.selectbox("Product_Type", ["Baking goods", "Bread", "Breakfast", "Canned", "Dairy", "Frozen foods", 
                                                       "Fruits and vegetables", "Hard drinks", "Health and hygiene",
                                                       "Household", "Meat", "Snack foods", "Soft drinks", "Seafood",
@@ -41,7 +41,8 @@ if st.button("Forecast", type='primary'):
         predicted_sales = result["Sales"]
         st.write(f"Predicted Product Store Sales Total: ₹{predicted_sales:.2f}")
     else:
-        st.error("Error in API request")
+        st.error(f"API error: {response.status_code}")
+        st.code(response.text)
 
 # Section for batch prediction
 st.subheader("Batch Forecast")
@@ -58,4 +59,5 @@ if uploaded_file is not None:
             st.success("Batch forecast completed!")
             st.write(predictions)  # Display the predictions
         else:
-            st.error("Unable to connect to the forecast API.")
+            st.error(f"API error: {response.status_code}")
+            st.code(response.text)
